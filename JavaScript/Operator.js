38f903a5c2e78efdf9840f7f77ee803f7ab5b67c
starlash7 <yang92sw@gmail.com>
2024-05-14 00:17:46 +0900

    Operator

---
+++ JavaScript/Operator.js
@@ -0,0 +1,37 @@
+let a = 10;
+let b = 20;
+
+console.log(a);
+// 출력 결과:10
+
+console.log(b);
+// 출력 결과:20
+
+let c = 2;
+let d = 4;
+
+console.log(c + d);
+// 출력 결과:6
+console.log(c - d);
+// 출력 결과:-2
+console.log(c * d);
+// 출력 결과:8
+console.log(c / d);
+// 출력 결과:0.5
+console.log(c % d);
+// 출력 결과:2
+console.log(c ** d);
+// 출력 결과:16
+
+c += d; // c = c + d
+console.log(c);
+// 출력 결과:6
+c -= d; // c = c - d
+console.log(c);
+// 출력 결과:2
+c *= d; // c = c * d
+console.log(c);
+// 출력 결과:8
+c /= d; // c = c / d
+console.log(c);
+// 출력 결과:2
