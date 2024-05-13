fc2e9b6f4582f06b011796c8e13c6e6f89ee4dba
starlash7 <yang92sw@gmail.com>
2024-05-14 00:02:10 +0900

    DataType

---
+++ JavaScript/DataType.js
@@ -0,0 +1,45 @@
+// Number type
+let n1 = 1234;
+let n2 = 5.678;
+
+// String type
+let s1 = "hello";
+let s2 = "world";
+
+// Boolean type
+let b1 = true;
+let b2 = false;
+
+// Null type
+let n = null;
+
+// Undefined type
+let u1;
+let u2 = undefined;
+
+// Array type(배열)
+let arr = [1, 2, 3, 4];
+
+// Number 타입으로만 이루어진 배열
+let arr1 = [1, 2, 3, 4, 5];
+// String 타입으로만 이루어진 배열
+let arr2 = ["h", "e", "l", "l", "o"];
+// Nuber 타입과 String 타입을 함께 사용한 배열
+let arr3 = [1, "h", 2, "i"];
+// 다양한 자료형들을 함께 사용한 배열
+let arr4 = [true, 1, undefined, false, "h", 2, null, "i"];
+
+console.log(arr1[0]);
+//출력 결과:1
+
+console.log(arr2[1]);
+//출력 결과:e
+
+console.log(arr3[2]);
+//출력 결과:2
+
+console.log(arr4[3]);
+//출력 결과:false
+
+// Object type(객체)
+let obj = { a: "apple", b: "banana", c: "carrot" };
